1e2e778209d54d3d8366f323d09e5b5f82097d83
unknown <SENA@ARMDFPCCIFSD024.FormacionSena.RED>
2022-02-08 14:54:43 -0500

    Inicio del proyecto Counter

---
+++ src/components2/UI/UIHeader/UIHeader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const UIHeader = () => {
+  return(
+      <div className='header'>
+          <h1>CounterApp</h1>
+          <div className='space'></div>
+          <div className='counter'>
+              <h2 id='paintCounnter'>0</h2>   
+          </div>
+      </div>
+  )
+};
---
+++ src/components2/UI/UIMain/UIMain.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import img from '../../../components/Img/logo512.png';
+
+export const UIMain = () => {
+  return (
+      <div className='image'>
+
+      </div>
+  );
+};
