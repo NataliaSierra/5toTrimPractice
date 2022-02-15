15dbaeaa91e199c37dab532a8f15795671bad3ab
sebastianmiracastro <smira44@misena.edu.co>
2022-02-14 22:32:30 -0500

    Estilos en Error404 hechos, y acomodando enrutamientos

--- src/components/UI/UINotFound/NotFound.jsx
+++ src/components/UI/UINotFound/NotFound.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import ErrorGif from '../../Img/404Error.gif'
+import '../../../App.css'
+import { NavLink } from 'react-router-dom';
+import UIButtons from '../UIButtons/UIButtons';
 
 export const NotFound = () => {
   return (
-    <div>
-        <h1>NotFound</h1>
-    </div>
+        <div className='containerNotFound'>
+            <img className='containerError404' src={ErrorGif} alt="ErrorImage404"></img>
+            <NavLink to="/"><UIButtons classButtons = "btnNotFound" nameButtons = "Volver a la pagina principal"></UIButtons></NavLink>
+        </div>
   )
 }
+
+export default NotFound;
